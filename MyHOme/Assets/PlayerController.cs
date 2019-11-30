5518538c0a3637820483b5a58d7b6f424f02b525
mnscontacts <mnscontacts@gmail.com>
2019-11-30 10:39:21 +0530

    v2
    all file uploaded

---
+++ MyHOme/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 20.0f;
+    public float rotationSpeed = 100.0f;
+    public float jumpHeight = 10f;
+
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * jumpHeight);
+        }
+    }
+}
